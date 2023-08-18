6f1aec74cd5f0386788e6fcd6ec2ac3d5118e273
NiNeDelixe <67333475+NiNeDelixe@users.noreply.github.com>
2023-08-18 18:17:39 +0300

    Delete SomAlwaysCleaner/vcpkg_installed directory

--- SomAlwaysCleaner/vcpkg_installed/x64-windows/vcpkg/blds/zlib/x64-windows-dbg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/alkor/source/repos/SomAlwaysCleaner/SomAlwaysCleaner/vcpkg_installed/x64-windows/vcpkg/blds/zlib/src/v1.2.13-f30d2a168d.clean
-# Build directory: C:/Users/alkor/source/repos/SomAlwaysCleaner/SomAlwaysCleaner/vcpkg_installed/x64-windows/vcpkg/blds/zlib/x64-windows-dbg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
